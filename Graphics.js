8aef52345fbe4385bc8ce1d3a4a8e7b4b4d638b5
mudale222 <yuziel@walla.co.il>
2020-01-20 00:00:10 +0200

    style and order added. mostly html divs.

--- Graphics.js
+++ Graphics.js
@@ -6,12 +6,12 @@ class Graphics {
         this.messages = [];
     }
     renderPieces() {
-        let allImages = document.querySelectorAll("img"); 
-        for (var i = 0; i < allImages.length; i++) 
+        let allImages = document.querySelectorAll("img");
+        for (var i = 0; i < allImages.length; i++)
             allImages[i].parentElement.removeChild(allImages[i]);
         for (let k = 0; k < this.symbolicBoard.length; k++) {
             for (let l = 0; l < this.symbolicBoard.length; l++) {
-                if (this.symbolicBoard[k][l] != null) 
+                if (this.symbolicBoard[k][l] != null)
                     this.divsBoard[k][l].appendChild(this.symbolicBoard[k][l].img);
             }
         }
@@ -19,15 +19,32 @@ class Graphics {
     renderDivsBoard() {
         document.querySelector("body").style.visibility = "visible";
         let isWhite = true;
+        let gameDiv = document.createElement("div"),
+            gameBoardDiv = document.createElement("div"),
+            panelDiv = document.createElement("div");
+        let header = document.createElement("h1");
+        header.innerText = "CHECKERS";
+        gameDiv.appendChild(header);
+
+
+        let header2 = document.createElement("h1");
+        header2.innerText = "asdfasdfasfda";
+        panelDiv.appendChild(header2);
+
+        gameDiv.id = "game", gameBoardDiv.id = "gameBoard", panelDiv.id = "panel";
+        insertElementToDOM("body", gameDiv);
+        gameDiv.appendChild(gameBoardDiv);
+        gameDiv.appendChild(panelDiv);
+        //insertElementToDOM("body", gameBoardDiv);
         for (let k = 0; k < this.symbolicBoard.length; k++) {
-            insertElementToDOM("body", document.createElement("br"));
+            gameBoardDiv.appendChild(document.createElement("br"));
             for (let l = 0; l < this.symbolicBoard.length; l++) {
                 this.divsBoard[k][l] = document.createElement("div");
                 isWhite = l != 0 ? !isWhite : isWhite;
-                this.divsBoard[k][l].id = isWhite ? "white" : "black";
+                this.divsBoard[k][l].classList = isWhite ? "white" : "black";
                 this.divsBoard[k][l].setAttribute("x", l);
                 this.divsBoard[k][l].setAttribute("y", k);
-                insertElementToDOM("body", this.divsBoard[k][l]);
+                gameBoardDiv.appendChild(this.divsBoard[k][l]);
             }
         }
     }
